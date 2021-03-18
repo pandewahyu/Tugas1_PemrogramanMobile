a7fe8c172a9c7e7c19b34db43c52b6818c154edd
pandewahyu <80252648+pandewahyu@users.noreply.github.com>
2021-03-18 17:07:43 +0800

    123

--- lib/detail_produk.dart
+++ lib/detail_produk.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 class DetailProduk extends StatefulWidget {
   DetailProduk(
-      {Key key, this.nama, this.deskripsi, this.harga, this.gambar, this.star})
+      {Key key, this.name, this.deskription, this.price, this.image, this.star})
       : super(key: key);
-  final String nama; 
-  final String deskripsi;
-  final int harga;
-  final String gambar;
+  final String name;
+  final String deskription;
+  final int price;
+  final String image;
   final int star;
   @override
   _DetailProdukState createState() => _DetailProdukState();
@@ -19,65 +19,58 @@ class _DetailProdukState extends State<DetailProduk> {
   Widget build(BuildContext context) {
     for (var i = 0; i < widget.star; i++) {
       childrenstar.add(new Icon(
-        Icons.star, 
-        size: 20, 
-        color: Colors.yellow[700],
-        ));
+        Icons.star_border_outlined,
+        size: 20,
+        color: Colors.yellow[400],
+      ));
     }
 
     return Scaffold(
       appBar: new AppBar(
-        title: new Text("${widget.nama}"),
-        backgroundColor: Colors.redAccent,
+        title: new Text("${widget.name}"),
+        backgroundColor: Colors.lightGreen[600],
       ),
+      backgroundColor: Colors.lightGreen[300],
       body: new Container(
         width: double.infinity,
         child: new ListView(
           children: <Widget>[
             Column(
               children: <Widget>[
-                new Image.asset("assets/appimages/" + widget.gambar)
+                new Image.asset("assets/appimages/" + widget.image)
+              ],
+            ),
+            Container(
+              decoration: new BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [const Color(0xff03a9f4), const Color(0xff01579b)],
+                ),
+              ),
+              padding: EdgeInsets.all(10.0),
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    children: childrenstar,
+                  ),
+                  new Container(
+                    child: new Text(
+                      "Rp. " + this.widget.price.toString(),
+                      style: new TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontFamily: "NeoSansBold"),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ],
         ),
-        Container(
-          decoration: new BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [const Color(0xffe53935), 
-              const Color(0xffc62828)],
-            ),
-          ),
-          padding: EdgeInsets.all(10.0),
-          child: new Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Row(
-                 children: childrenstar,
-              ),
-              new Container(
-                child: new Text(
-                  "Rp. " + this.widget.harga.toString(),
-                  style: new TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontFamily: "NeoSansBold"),
-                ),
-              )
-            ],
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.all(10),
-          color: Colors.redAccent,
-          width: double.infinity,
-          height: double.maxFinite,
-          child: new Text(widget.deskripsi),
-        )
-      ],
-    ),
-  ),
-);
-}
+      ),
+    );
+  }
 }
